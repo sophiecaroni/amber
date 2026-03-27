eab4cb99ebb3902bc28ab8b9f8ba6fa3a1f4ff30
sophiecaroni <sophie.caroni@gmail.com>
2026-03-27 20:21:40 +0100

    Add run script for feature extraction pipeline

---
+++ scripts/run_tables_creation.py
@@ -0,0 +1,44 @@
+import amber_utils.io_utils as io
+from src.amber.extract_features import extract_performance_features, extract_attention_features, extract_all_sessions
+
+
+def extract_all_session_perf_table(test: bool, save: bool = False):
+    all_session_perf_df = extract_all_sessions(extract_performance_features, test)
+
+    if save:
+        out_dir = io.get_results_path()
+        fname = 'performance_summary.csv'
+        all_session_perf_df.to_csv(out_dir / fname)
+        print(f'\n\tExporting {fname}...\n\tSaved ✅')
+    else:
+        print(
+            f"\nFinal df: \n\t{all_session_perf_df}"
+        )
+
+
+def extract_all_session_att_table(test: bool, save: bool = False):
+    all_session_att_df = extract_all_sessions(extract_attention_features, test)
+
+    if save:
+        out_dir = io.get_results_path()
+        fname = 'attention_features.csv'
+        all_session_att_df.to_csv(out_dir / fname)
+        print(f'\n\tExporting {fname}...\n\tSaved ✅')
+    else:
+        print(
+            f"\nFinal df: \n\t{all_session_att_df}"
+        )
+
+
+def extract_tables(**kwargs) -> None:
+    extract_all_session_perf_table(**kwargs)
+    extract_all_session_att_table(**kwargs)
+
+
+if __name__ == '__main__':
+    extract_tables(
+        test=False,
+        save=True,
+    )
+
+
