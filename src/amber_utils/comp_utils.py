bd77f6c0ced4f25a126a76cb949643c7605bc6f3
sophiecaroni <sophie.caroni@gmail.com>
2026-04-09 17:04:24 +0200

    Add parameter to optionally use median in mask_in_bounds_rows computation of top bound

--- src/amber_utils/comp_utils.py
+++ src/amber_utils/comp_utils.py
@@ -14,14 +14,21 @@ def s_to_ms(s_series: pd.Series) -> pd.Series:
     raise TypeError(f'pd.Series {s_series} should be of numeric type!')
 
 
-def mask_in_bounds_rows(col: pd.Series, n_std_top: float, bottom_bound: int = 200) -> pd.Series:
+def mask_in_bounds_rows(
+        col: pd.Series,
+        n_std_top: float,
+        bottom_bound: int = 200,
+        top_use_median: bool = False
+) -> pd.Series:
     """
     Return a boolean mask for rows within RT bounds.
-    Upper bound is defined as mean + std_top * std; lower bound is fixed at bottom_bound.
+    Top bound is defined as central_tendency + std_top * std; lower bound is fixed at bottom_bound.
     :param col: Series of reaction times in milliseconds.
-    :param n_std_top: Number of standard deviations above the mean to use as upper bound.
+    :param n_std_top: Number of standard deviations above the mean to use as top bound.
     :param bottom_bound: Minimum RT in milliseconds (default: 200ms).
+    :param top_use_median: If True, use median instead of mean for the top bound (default: False).
     :return: Boolean Series, True where RT is within bounds.
     """
-    top_bound = col.mean() + n_std_top * col.std()  # reaction time < rt_top_bound (i.e. < std_top*std)
+    center = col.median() if top_use_median else col.mean()
+    top_bound = center + n_std_top * col.std()
     return (bottom_bound <= col) & (col < top_bound)
