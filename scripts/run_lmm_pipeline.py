5b431aba3036fd872fe9e05a1be434124c8007e4
sophiecaroni <sophie.caroni@gmail.com>
2026-05-25 17:38:23 +0200

    Remove unused

--- scripts/run_lmm_pipeline.py
+++ scripts/run_lmm_pipeline.py
@@ -12,19 +12,11 @@ from amber.viz.lmm_plots import plot_lmm_predictions
 
 
 def run_lmm_pipeline(save: bool = False, show: bool = True, verbose: bool = False) -> None:
-    metrics = (
-        "rt_med",
-        "rt_mean",
-        "rt_med_log",
-        "rt_mean_log",
-    )
-
-    for metric in metrics:
-        print(f"\n####################################### {metric} #######################################\n")
-        # fit_method = lmm.select_best_fit_method(rt_metric_str=metric)
-        lmm.test(rt_metric_col=metric, verbose=verbose, save=save)
-        plot_lmm_predictions(metric=metric, save=save, show=show)
-        lmm.post_hocs(rt_metric_col=metric, verbose=verbose, save=save)
+    metric = 'rt_med_log'
+    print(f"\n####################################### {metric} #######################################\n")
+    lmm.test(rt_metric_col=metric, verbose=verbose, save=save)
+    plot_lmm_predictions(metric=metric, save=save, show=show)
+    lmm.post_hocs(rt_metric_col=metric, verbose=verbose, save=save)
 
 
 if __name__ == "__main__":
