5e3dd3d496129956a2f1f1270f65ba2a3efd97b4
sophiecaroni <sophie.caroni@gmail.com>
2026-05-28 17:40:39 +0200

    Add features normality checks to attention LMM pipeline

--- src/amber/analysis/att_lmm_pipeline.R
+++ src/amber/analysis/att_lmm_pipeline.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyr)
+library(car)
 
 # -------------------------
 # 0. Setup and retrieve args
@@ -66,3 +67,17 @@ df <- df %>%
     select(-att_score_bl, -interv_eff, -att_score) %>%  # remove cols anymore needed
     drop_na(att_change)  # nans appear when the patient did not complete one of the sessions needed for computation
 
+# Define y
+y <- df$att_change
+
+# Check normality of the dependent variable via a QQ plot
+if (save) {
+    png(file.path(figures_dir, paste0("qq", "_", att_metric, ".png")))
+    qqPlot(y)
+    invisible(dev.off())  # write file and close figure
+}
+if (save) {
+    png(file.path(figures_dir, paste0("hist", "_", att_metric, ".png")))
+    hist(y)
+    invisible(dev.off())  # write file and close figure
+}
