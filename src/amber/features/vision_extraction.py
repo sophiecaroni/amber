43c8845dc12d68ef952d394420cb0d16e3fa666a
sophiecaroni <sophie.caroni@gmail.com>
2026-06-23 09:15:29 +0200

    Assign 'both' eye condition to stereovision test data

--- src/amber/features/vision_extraction.py
+++ src/amber/features/vision_extraction.py
@@ -22,7 +22,7 @@ def _format_stereo_df(df_label: str, df: pd.DataFrame) -> pd.DataFrame:
         raise ValueError('Unexpected format of acq sheet name!')
     df['vis_type'] = 'ste'
     df['vis_test'] = parts[1].lower()[:3]  # only encode via three first letters
-    df['eye_cond'] = None  # stereovision tests are conducted on both eyes
+    df['eye_cond'] = 'both'  # stereovision tests are conducted on both eyes
     return df
 
 
